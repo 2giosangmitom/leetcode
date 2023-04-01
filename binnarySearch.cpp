553689b5452793c4d044a42209d8630fa724b5c2
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-04-01 08:19:12 +0700

    update

--- binnarySearch.cpp
+++ binnarySearch.cpp
@@ -16,4 +16,23 @@ int binarySearch(int arr[], int left, int right, int x) {
   }
   return -1;
 }
-int main() {}
+
+int main() {
+  int n;
+  cout << "Length of arr = ";
+  cin >> n;
+  int arr[n];
+  for (int i = 0; i <= n - 1; i++) {
+    cout << "arr[" << i << "] = ";
+    cin >> arr[i];
+  }
+  int target;
+  cout << "Search: ";
+  cin >> target;
+  int result = binarySearch(arr, 0, n - 1, target);
+  if (result == -1) {
+    cout << "Can't find " << target;
+  } else {
+    cout << "Position is " << result;
+  }
+}
