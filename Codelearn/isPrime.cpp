561167f9f1523ac691274a3e28a5c8e51414defc
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-04-04 21:38:56 +0700

    update

--- Codelearn/isPrime.cpp
+++ Codelearn/isPrime.cpp
@@ -6,11 +6,11 @@ bool isPrime(int n) {
   if (n < 2) {
     return false;
   }
-  if (n == 2) {
-    return true;
-  }
   if (n % 2 == 0) {
     return false;
+  }
+  if (n == 2) {
+    return true;
   }
   for (int i = 3; i < n / 2; i += 2) {
     if (n % i == 0) {
