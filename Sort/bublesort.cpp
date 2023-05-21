192800ca338da49b3f59798343120f67f7d1c02b
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-05-21 21:05:46 +0700

    update

--- Sort/bublesort.cpp
+++ Sort/bublesort.cpp
@@ -3,9 +3,8 @@ using namespace std;
 
 void bubbleSort(int arr[], int n) {
   bool haveSwap = false;
-  for (int i = 0; i < n - 1; i++) {
-    haveSwap = false;
-    for (int j = 0; j < n - i - 1; j++) {
+  for (int i = 0; i < n - 1; ++i) {
+    for (int j = 0; j < n - i - 1; ++j) {
       if (arr[j] > arr[j + 1]) {
         swap(arr[j], arr[j + 1]);
         haveSwap = true;
