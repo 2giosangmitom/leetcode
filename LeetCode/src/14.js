ac451a1fb0fa73bd6034436a3da9c033b4fc7a0e
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-05-21 20:42:06 +0700

    add 14: Longest Common Prefix

---
+++ LeetCode/src/14.js
@@ -0,0 +1,18 @@
+// Longest Common Prefix
+const longestCommonPrefix = function (strs) {
+	if (strs.length === 0) {
+		return "";
+	}
+	let prefix = strs[0];
+	for (let i = 1; i < strs.length; ++i) {
+		while (strs[i].indexOf(prefix) !== 0) {
+			prefix = prefix.slice(0, prefix.length - 1);
+			if (prefix === "") {
+				return "";
+			}
+		}
+	}
+	return prefix;
+};
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
