b0b39b42a59525558c8e274ac5fe772b1b8817f0
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-05-21 08:42:47 +0700

    add 268: Missing number

---
+++ LeetCode/src/268.js
@@ -0,0 +1,8 @@
+const missingNumber = function (nums) {
+	const total = (nums.length * (nums.length + 1)) / 2;
+	let sum = 0;
+	nums.forEach((value) => {
+		sum += value;
+	});
+	return total - sum;
+};
