c5729cd87184041685dc7de59320e7cba30fd6b6
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-05-23 22:13:41 +0700

    50: Pow(x, n)

--- LeetCode/src/50.cpp
+++ LeetCode/src/50.cpp
@@ -1,18 +1,21 @@
 // Pow(x, n)
+#include <cmath>
 #include <iostream>
 using namespace std;
 
 class Solution {
 public:
   double myPow(double x, int n) {
-    double pow = n;
     if (n == 0) {
       return 1;
     }
-    if (n < 0) {
-      pow = -1.0 * n;
+    int pow = abs(n);
+    double result;
+    if (pow % 2 == 0) {
+      result = myPow(x * x, pow / 2);
+    } else {
+      result = myPow(x * x, (pow - 1) / 2) * x;
     }
-    double result = n % 2 == 0 ? myPow(x * x, pow / 2) : myPow(x * x, (pow - 1) / 2) * x;
     return n < 0 ? 1 / result : result;
   }
 };
--- LeetCode/src/50.js
+++ LeetCode/src/50.js
@@ -2,6 +2,13 @@
 const myPow = function (x, n) {
 	if (n === 0) return 1;
 	const pow = Math.abs(n);
-	const result = pow % 2 === 0 ? myPow(x * x, pow / 2) : myPow(x * x, (pow - 1) / 2) * x;
+	let result;
+	if (pow % 2 === 0) {
+		result = myPow(x * x, pow / 2);
+	} else {
+		result = myPow(x * x, (pow - 1) / 2) * x;
+	}
 	return n < 0 ? 1 / result : result;
 };
+
+console.log(myPow(2, 2));
