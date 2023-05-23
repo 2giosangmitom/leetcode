2b3d91be253ac6c96ecd3baf63d80e1bff6ed71b
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-05-23 14:09:16 +0700

    update

--- LeetCode/src/704.cpp
+++ LeetCode/src/704.cpp
@@ -5,9 +5,9 @@ using namespace std;
 class Solution {
 public:
   int search(vector<int> &nums, int target) {
-    int left = 0, right = (int)nums.size() - 1;
+    int left = 0, right = nums.size() - 1, mid;
     while (left <= right) {
-      int mid = left + (right - left) / 2;
+      mid = (left + right) / 2;
       if (nums[mid] == target)
         return mid;
       else if (nums[mid] < target)
