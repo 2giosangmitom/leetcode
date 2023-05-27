0840e54d12e0a64fec737bde3e567babceccb2f6
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-05-27 11:28:34 +0700

    Missing Number

---
+++ LeetCode/src/268.ts
@@ -0,0 +1,9 @@
+// Missing Number
+function missingNumber(nums: number[]): number {
+	let sum = 0;
+	const total = (nums.length * (nums.length + 1)) / 2;
+	nums.forEach((value) => {
+		sum += value;
+	});
+	return total - sum;
+}
