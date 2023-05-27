4e98385302e9bd8a596b6618411dcdfd20717607
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-05-27 10:19:44 +0700

    update

--- LeetCode/src/20.ts
+++ LeetCode/src/20.ts
@@ -1,20 +1,20 @@
 // Valid Parentheses
-const isValid = function (s) {
+function isValid(s: string): boolean {
 	if (s.length % 2 !== 0) {
 		return false;
 	}
-	const stack = [];
+	const stack: string[] = [];
 	for (const char of s) {
-		if (char === "(" || char === "{" || char === "[") {
+		if (char === "{" || char === "[" || char === "(") {
 			stack.push(char);
 		} else {
 			if (stack.length === 0) {
 				return false;
-			} else if (char === ")" && stack[stack.length - 1] === "(") {
-				stack.pop();
 			} else if (char === "}" && stack[stack.length - 1] === "{") {
 				stack.pop();
 			} else if (char === "]" && stack[stack.length - 1] === "[") {
+				stack.pop();
+			} else if (char === ")" && stack[stack.length - 1] === "(") {
 				stack.pop();
 			} else {
 				return false;
@@ -22,4 +22,4 @@ const isValid = function (s) {
 		}
 	}
 	return stack.length === 0;
-};
+}
