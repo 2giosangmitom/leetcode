00f1212ede49acfa6bd436b9ebce19b13ec877fa
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-05-28 21:36:26 +0700

    Valid Palindrome

---
+++ LeetCode/src/125.ts
@@ -0,0 +1,7 @@
+// Valid Palindrome
+function isPalindrome(s: string): boolean {
+	if (s.length === 0) return true;
+	const newStr = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+	const reversedStr = newStr.split("").reverse().join("").toLowerCase();
+	return newStr === reversedStr;
+}
