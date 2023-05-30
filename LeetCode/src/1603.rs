a5315b259f869a7f923dd68624aab361c312810e
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-05-30 15:25:23 +0700

    update

---
+++ LeetCode/src/1603.rs
@@ -0,0 +1,27 @@
+// Design Parking System
+struct ParkingSystem {
+    big: i32,
+    medium: i32,
+    small: i32,
+}
+
+impl ParkingSystem {
+    fn new(big: i32, medium: i32, small: i32) -> Self {
+        Self { big, medium, small }
+    }
+
+    fn add_car(&mut self, car_type: i32) -> bool {
+        if car_type == 1 && self.big > 0 {
+            self.big -= 1;
+            return true;
+        } else if car_type == 2 && self.medium > 0 {
+            self.medium -= 1;
+            return true;
+        } else if car_type == 3 && self.small > 0 {
+            self.small -= 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
