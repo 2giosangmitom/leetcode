8184df7b13a702e29a48bf7b260d34a0b9f10141
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-05-30 14:57:15 +0700

    update

---
+++ LeetCode/src/121.rs
@@ -0,0 +1,18 @@
+// Best Time to Buy and Sell Stock
+use std::cmp;
+
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut max_profit = 0;
+        let mut buy_day = 0;
+        for sell_day in 1..prices.len() {
+            if prices[buy_day] < prices[sell_day] {
+                let profit = prices[sell_day] - prices[buy_day];
+                max_profit = cmp::max(max_profit, profit);
+            } else {
+                buy_day = sell_day;
+            }
+        }
+        return max_profit;
+    }
+}
---
+++ LeetCode/src/1603.ts
@@ -0,0 +1,21 @@
+// Design Parking System
+class ParkingSystem {
+	big: number;
+	medium: number;
+	small: number;
+	constructor(big: number, medium: number, small: number) {
+		this.big = big;
+		this.medium = medium;
+		this.small = small;
+	}
+
+	addCar(carType: number): boolean {
+		if (carType === 1) {
+			return this.big-- > 0;
+		} else if (carType === 2) {
+			return this.medium-- > 0;
+		} else {
+			return this.small-- > 0;
+		}
+	}
+}
