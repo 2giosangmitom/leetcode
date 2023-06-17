fbe360dfeba1ae6500b5beb0380dbfdbd16db599
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-06-17 13:25:45 +0700

    update

--- LeetCode/src/1.ts
+++ LeetCode/src/1.ts
@@ -1,12 +1,14 @@
 // Two Sum
-function twoSum(nums: number[], target: number): number[] {
-	const hash: { [index: number]: number } = {};
-	for (let i = 0; i < nums.length; ++i) {
-		const need: number = target - nums[i]; // 2
-		if (hash[need] !== undefined) {
-			return [hash[need], i];
-		}
-		hash[nums[i]] = i;
-	}
-	return [-1];
+function twoSum(nums: number[], target: number): number[] | number {
+  const hashMap = new Map<number, number>();
+  for (let i = 0; i < nums.length; ++i) {
+    const need = target - nums[i];
+    if (hashMap.has(need)) {
+      return [hashMap.get(need), i];
+    }
+    hashMap.set(nums[i], i);
+  }
+  return -1;
 }
+
+console.log(twoSum([2, 7, 11, 15], 9));
