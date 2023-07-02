e0811989c702574d3c7ff8465c74fba51d16141a
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-07-02 13:59:04 +0700

    Two Sum

--- LeetCode/src/1.java
+++ LeetCode/src/1.java
@@ -7,20 +7,20 @@ class Solution {
     for (var i = 0; i < nums.length; ++i) {
       var needNumber = target - nums[i];
       if (hashMap.containsKey(needNumber)) {
-        return new int[] { hashMap.get(needNumber), i };
+        return new int[] {hashMap.get(needNumber), i};
       }
       hashMap.put(nums[i], i);
     }
-    return new int[-1];
+    return new int[] {-1};
   }
 
   public static void main(String[] args) {
     var s = new Solution();
-    int[] nums = { 2, 7, 11, 15 };
+    int[] nums = {2, 7, 11, 15};
     System.out.println(Arrays.toString(s.twoSum(nums, 9))); // TEST: => [0, 1]
-    int[] nums2 = { 3, 2, 4 };
+    int[] nums2 = {3, 2, 4};
     System.out.println(Arrays.toString(s.twoSum(nums2, 6))); // TEST: => [1, 2]
-    int[] nums3 = { 3, 3 };
+    int[] nums3 = {3, 2};
     System.out.println(Arrays.toString(s.twoSum(nums3, 6))); // TEST: => [0, 1]
   }
 }
