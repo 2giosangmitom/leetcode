37bfd7249337c63aa40d42a401ae6a164fe1a5b5
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-08-12 16:03:32 +0700

    refactor code

--- Rust/src/num_of_good_pairs.rs
+++ Rust/src/num_of_good_pairs.rs
@@ -11,15 +11,16 @@ pub trait NumOfGoodPairs {
 
 impl NumOfGoodPairs for Solution {
     fn num_identical_pairs(nums: Vec<i32>) -> i32 {
+        use std::collections::hash_map;
         use std::collections::HashMap;
         let mut result = 0;
         let mut hash_map: HashMap<i32, i32> = HashMap::new();
         for num in nums.into_iter() {
-            if hash_map.contains_key(&num) {
+            if let hash_map::Entry::Vacant(e) = hash_map.entry(num) {
+                e.insert(1);
+            } else {
                 result += *hash_map.get_mut(&num).unwrap();
                 *hash_map.get_mut(&num).unwrap() += 1;
-            } else {
-                hash_map.insert(num, 1);
             }
         }
         result
