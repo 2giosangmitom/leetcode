966da9b32a2053935ec70b75509cc35cf7148f01
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-08-27 08:55:23 +0700

    update

---
+++ CSharp/two_sum/Tests.cs
@@ -0,0 +1,16 @@
+namespace two_sum {
+    public class Test_TwoSum {
+        [Fact]
+        public void Test1() {
+            int[] got = Solution.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+            Assert.Equal(new int[] { 0, 1 }, got);
+        }
+
+        [Fact]
+        public void Test2() {
+            int[] got = Solution.TwoSum(new int[] { 3, 2, 4 }, 6);
+            Assert.Equal(new int[] { 1, 2 }, got);
+
+        }
+    }
+}
