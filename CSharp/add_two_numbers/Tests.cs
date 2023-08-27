72fe33e8f34273a3aeb6785a25bd1850b9aedfdb
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-08-27 13:48:09 +0700

    update

--- CSharp/add_two_numbers/Tests.cs
+++ CSharp/add_two_numbers/Tests.cs
@@ -5,8 +5,8 @@ namespace add_two_numbers {
             var l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
             var l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
             var want = new ListNode(7, new ListNode(0, new ListNode(8)));
-            var result = Solution.AddTwoNumbers(l1, l2);
-            Assert.Equivalent(want, result);
+            var got = Solution.AddTwoNumbers(l1, l2);
+            Assert.Equivalent(want, got);
         }
 
         [Fact]
@@ -14,8 +14,8 @@ namespace add_two_numbers {
             var l1 = new ListNode(0);
             var l2 = new ListNode(0);
             var want = new ListNode(0);
-            var result = Solution.AddTwoNumbers(l1, l2);
-            Assert.Equivalent(want, result);
+            var got = Solution.AddTwoNumbers(l1, l2);
+            Assert.Equivalent(want, got);
         }
     }
 }
