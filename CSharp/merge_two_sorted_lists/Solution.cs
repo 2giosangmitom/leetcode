2a9928022186ef7f52bc4c969980dc2ee2bea514
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-08-28 08:51:11 +0700

    update

---
+++ CSharp/merge_two_sorted_lists/Solution.cs
@@ -0,0 +1,31 @@
+namespace merge_two_sorted_lists {
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution {
+        public static ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+            var head = new ListNode();
+            var curr = head;
+
+            while (list1 != null && list2 != null) {
+                if (list1.val < list2.val) {
+                    curr.next = list1;
+                    list1 = list1.next;
+                } else {
+                    curr.next = list2;
+                    list2 = list2.next;
+                }
+                curr = curr.next;
+            }
+            curr.next = list1 ?? list2;
+
+            return head.next;
+        }
+    }
+}
---
+++ CSharp/merge_two_sorted_lists/Tests.cs
@@ -0,0 +1,12 @@
+namespace merge_two_sorted_lists {
+    public class Test_MergeTwoSortedLists {
+        [Fact]
+        public void Test1() {
+            var list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+            var list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+            var got = Solution.MergeTwoLists(list1, list2);
+            var want = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(4))))));
+            Assert.Equivalent(want, got);
+        }
+    }
+}
