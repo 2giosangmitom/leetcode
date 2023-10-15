e2d8b974d335dc761fb191d53b8232c09863b331
Vo Quang Chien <voquangchien.dev@proton.me>
2023-10-15 09:59:11 +0700

    ruby: format

--- Ruby/lib/two_sum.rb
+++ Ruby/lib/two_sum.rb
@@ -8,7 +8,6 @@ def two_sum(nums, target)
   nums.each_with_index do |num, i|
     complement = target - num
     return [map[complement], i] if map.key?(complement)
-
     map[num] = i
   end
   [-1]
