bd3e263bb044ffbeef5fa6a2eda65ebf6e7a7b73
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-10-18 20:24:12 +0700

    add Ruby

---
+++ Ruby/lib/two_sum.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+  map = {}
+  nums.each_with_index do |num, i|
+    complement = target - num
+    return [map[complement], i] if map.key?(complement)
+
+    map[num] = i
+  end
+  [-1]
+end
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum2(nums, target)
+  for i in 0...nums.length do
+    complement = target - nums[i]
+    for j in i + 1...nums.length do
+      return [i, j] if complement == nums[j]
+    end
+  end
+  [-1]
+end
