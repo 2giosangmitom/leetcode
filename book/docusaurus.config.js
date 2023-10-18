2da78b7e7c3c3cc236bcc7782e0a19de15516555
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-10-18 19:46:51 +0700

    book: update config

--- book/docusaurus.config.js
+++ book/docusaurus.config.js
@@ -1,5 +1,4 @@
 // @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
 
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
@@ -9,18 +8,10 @@ const config = {
   title: "Leet Code With Test",
   tagline: "📚 Solution for all LeetCode problems with unit test",
   favicon: "/img/favicon.ico",
-
-  // Set the production url of your site here
   url: "https://2giosangmitom.github.io",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/leetcode/",
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "2giosangmitom", // Usually your GitHub org/user name.
-  projectName: "leetcode", // Usually your repo name.
-
+  organizationName: "2giosangmitom",
+  projectName: "leetcode",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
@@ -53,12 +44,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: "My Site",
+        title: "LeetCode With Test",
         logo: {
-          alt: "My Site Logo",
+          alt: "Logo",
           src: "img/logo.svg",
         },
         items: [
