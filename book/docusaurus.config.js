7c2a69e490f9d2faa32440d5299b65168988b0d4
Vo Quang Chien <voquangchien.dev@proton.me>
2023-10-18 17:02:30 +0700

    docs: moved to docusaurus

---
+++ book/docusaurus.config.js
@@ -0,0 +1,90 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Leet Code With Test",
+  tagline: "📚 Solution for all LeetCode problems with unit test",
+  favicon: "/img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://2giosangmitom.github.io",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/leetcode/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "2giosangmitom", // Usually your GitHub org/user name.
+  projectName: "leetcode", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+        },
+        blog: {
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: "img/docusaurus-social-card.jpg",
+      navbar: {
+        title: "My Site",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Tutorial",
+          },
+          { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://github.com/2giosangmitom/leetcode",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        copyright: `Copyright © ${new Date().getFullYear()} LeetCode With Test. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
--- book/src/README.md
+++
@@ -1,9 +0,0 @@
-# Introduction
-
-**LeetCode** is a platform that helps users improve their programming skills through interview questions commonly used at the world's leading technology companies.
-
-The essence of **LeetCode** is to provide IT knowledge and this knowledge is often used at **Google, Amazon, Microsoft, Meta, Netflix**...
-
-**LeetCode** is a good place for programmers to test their algorithm skills. It's a tool to support knowledge improvement, not really a tool to help you with "tricks" to pass the interview round.
-
-This repository contains several solutions to **LeetCode** problems written in [Rust](https://www.rust-lang.org/). You can refer to it if you haven't found a solution yet.
