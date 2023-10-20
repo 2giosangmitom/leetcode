1e1f87b4f6debadc4d6c93c4abdc314feee25ba4
Vo Quang Chien <voquangchien.dev@proton.me>
2023-10-20 12:24:50 +0700

    c#: add 2

---
+++ CSharp/AddTwoNumbers/Solutions.cs
@@ -0,0 +1,41 @@
+namespace CSharp.AddTwoNumbers;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode dummyHead = new(0);
+        ListNode tail = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0)
+        {
+            int digit1 = l1 != null ? l1.val : 0;
+            int digit2 = l2 != null ? l2.val : 0;
+            l1 = l1?.next;
+            l2 = l2?.next;
+
+            int sum = digit1 + digit2 + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            ListNode newNode = new(digit, null);
+            tail.next = newNode;
+            tail = tail.next;
+        }
+
+        return dummyHead.next;
+    }
+}
