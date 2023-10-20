55295448aa60bd9d111933cf33845e2aab65423f
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-10-20 12:31:50 +0700

    c#: add more test

--- CSharp/AddTwoNumbers/Solutions.cs
+++ CSharp/AddTwoNumbers/Solutions.cs
@@ -5,7 +5,7 @@ public class ListNode
     public int val;
     public ListNode next;
 
-    public ListNode(int val = 0, ListNode next = null)
+    public ListNode(int val, ListNode next = null)
     {
         this.val = val;
         this.next = next;
--- CSharp/AddTwoNumbers/Test.cs
+++ CSharp/AddTwoNumbers/Test.cs
@@ -12,6 +12,36 @@ public class Test
                     l1: new ListNode(2, new ListNode(4, new ListNode(3))),
                     l2: new ListNode(5, new ListNode(6, new ListNode(4))),
                     want: new ListNode(7, new ListNode(0, new ListNode(8)))
+                ),
+                (l1: new ListNode(0), l2: new ListNode(0), want: new ListNode(0)),
+                (
+                    l1: new ListNode(
+                        9,
+                        new ListNode(
+                            9,
+                            new ListNode(
+                                9,
+                                new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))
+                            )
+                        )
+                    ),
+                    l2: new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))),
+                    want: new ListNode(
+                        8,
+                        new ListNode(
+                            9,
+                            new ListNode(
+                                9,
+                                new ListNode(
+                                    9,
+                                    new ListNode(
+                                        0,
+                                        new ListNode(0, new ListNode(0, new ListNode(1)))
+                                    )
+                                )
+                            )
+                        )
+                    )
                 )
             };
 
