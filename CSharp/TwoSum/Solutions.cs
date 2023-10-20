723261c85184e1cc3c1e1aba1f6e4674591bf7b5
Vo Quang Chien <voquangchien.dev@proton.me>
2023-10-20 12:24:33 +0700

    c#: refactor

---
+++ CSharp/TwoSum/Solutions.cs
@@ -0,0 +1,38 @@
+namespace CSharp.TwoSum;
+
+public class Solution
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> map = new();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (map.ContainsKey(complement))
+            {
+                return new int[] { map[complement], i };
+            }
+            else
+            {
+                map.Add(nums[i], i);
+            }
+        }
+        return new int[] { -1 };
+    }
+
+    public static int[] TwoSum2(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (complement == nums[j])
+                {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] { -1 };
+    }
+}
