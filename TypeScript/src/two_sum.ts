e1fc272e1f1dfc56cbc977b7c53dd3f21208dea9
Vo Quang Chien <voquangchien.dev@proton.me>
2023-10-22 22:19:23 +0700

    ts: refactor 1

--- TypeScript/src/two_sum.ts
+++ TypeScript/src/two_sum.ts
@@ -1,11 +1,13 @@
 function twoSum(nums: number[], target: number): number[] {
-  const map = new Map();
+  const map = new Map<number, number>();
   for (let i = 0; i < nums.length; i++) {
     const complement = target - nums[i];
-    if (map.has(complement)) {
-      return [map.get(complement), i];
+    const v = map.get(complement);
+    if (v !== undefined) {
+      return [v, i];
+    } else {
+      map.set(nums[i], i);
     }
-    map.set(nums[i], i);
   }
   return [-1];
 }
