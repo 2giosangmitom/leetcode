ea15b2f6d488daf52d3a9c2667f57bd888b2bcb3
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-10-22 22:50:36 +0700

    c#: add 13

---
+++ CSharp/RomanToInteger/Solutions.cs
@@ -0,0 +1,36 @@
+namespace CSharp.RomanToInteger;
+
+public class Solution {
+    public static int RomanToInt(string s) {
+        Dictionary<char, int> romanMap =
+            new()
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 },
+            };
+
+        int result = 0;
+
+        foreach (char c in s.Reverse()) {
+            if (romanMap.TryGetValue(c, out int value)) {
+                int number = value;
+                if (value * 4 < result) {
+                    result -= number;
+                }
+                else {
+                    result += number;
+                }
+            }
+            else {
+                return -1;
+            }
+        }
+
+        return result;
+    }
+}
