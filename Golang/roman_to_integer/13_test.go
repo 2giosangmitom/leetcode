fcaee8cabd94f960eba975e7fb450608fe25eb33
Vo Quang Chien <voquangchien.dev@proton.me>
2023-10-22 23:32:40 +0700

    go: add 13

---
+++ Golang/roman_to_integer/13_test.go
@@ -0,0 +1,28 @@
+package romantointeger
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_13(t *testing.T) {
+	cases := []struct {
+		roman string
+		want  int
+	}{
+		{roman: "III", want: 3},
+		{roman: "LVIII", want: 58},
+		{roman: "MCMXCIV", want: 1994},
+		{roman: "XXIV", want: 24},
+		{roman: "LLVMR", want: -1},
+	}
+
+	for i, tt := range cases {
+		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
+			result := romanToInt(tt.roman)
+			assert.Equal(t, tt.want, result)
+		})
+	}
+}
