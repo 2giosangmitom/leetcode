d6375eef61875a844da5aed81bbecf06de243a67
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-11-04 19:06:05 +0700

    add java

---
+++ Java/src/main/java/TwoSum.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+class Solution {
+  public static int[] twoSum(int[] nums, int target) {
+    HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+    for (int i = 0; i < nums.length; ++i) {
+      int complement = target - nums[i];
+      if (hashMap.containsKey(complement)) {
+        return new int[] {hashMap.get(complement), i};
+      } else {
+        hashMap.put(nums[i], i);
+      }
+    }
+    return new int[] {-1};
+  }
+}
