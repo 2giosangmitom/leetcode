51dca01a3ca3518e3a4af288015ae4dc405f3520
Vo Quang Chien <voquangchien.dev@proton.me>
2023-11-05 13:58:19 +0700

    java: rename class

--- Java/src/test/java/TwoSumTests.java
+++ Java/src/test/java/TwoSumTests.java
@@ -7,28 +7,28 @@ class TwoSumTests {
   @Test
   @DisplayName("case 1")
   void test_1() {
-    int[] result = Solution.twoSum(new int[] {2, 7, 11, 15}, 9);
+    int[] result = TwoSum.twoSum(new int[] {2, 7, 11, 15}, 9);
     assertArrayEquals(new int[] {0, 1}, result);
   }
 
   @Test
   @DisplayName("case 2")
   void test_2() {
-    int[] result = Solution.twoSum(new int[] {3, 2, 4}, 6);
+    int[] result = TwoSum.twoSum(new int[] {3, 2, 4}, 6);
     assertArrayEquals(new int[] {1, 2}, result);
   }
 
   @Test
   @DisplayName("case 3")
   void test_3() {
-    int[] result = Solution.twoSum(new int[] {3, 3}, 6);
+    int[] result = TwoSum.twoSum(new int[] {3, 3}, 6);
     assertArrayEquals(new int[] {0, 1}, result);
   }
 
   @Test
   @DisplayName("case 4")
   void test_4() {
-    int[] result = Solution.twoSum(new int[] {2, 3, 4, 1, 25, 8}, 30);
+    int[] result = TwoSum.twoSum(new int[] {2, 3, 4, 1, 25, 8}, 30);
     assertArrayEquals(new int[] {-1}, result);
   }
 }
