1ff4f5aa8c55f8c8c1169f3534cddf5309eb2140
Vo Quang Chien <voquangchien.dev@proton.me>
2023-11-05 14:46:13 +0700

    chore: format java

--- Java/src/test/java/ReverseIntegerTests.java
+++ Java/src/test/java/ReverseIntegerTests.java
@@ -1,13 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class ReverseIntegerTests {
-  @ParameterizedTest(name = "reverse({0})")
-  @CsvSource({"123,321", "-123,-321", "120,21", "1534236469,0", "-2147483648,0", "900000,9"})
-  void test(int x, int want) {
-    int result = ReverseInteger.reverse(x);
-    assertEquals(want, result);
-  }
+    @ParameterizedTest(name = "reverse({0})")
+    @CsvSource({"123,321", "-123,-321", "120,21", "1534236469,0", "-2147483648,0", "900000,9"})
+    void test(int x, int want) {
+        int result = ReverseInteger.reverse(x);
+        assertEquals(want, result);
+    }
 }
