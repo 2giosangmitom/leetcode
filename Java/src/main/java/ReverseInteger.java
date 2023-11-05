4e5a612e073d084e2d60a53ce5a3b8382d0077e2
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-11-05 21:26:19 +0700

    chore(java): format

--- Java/src/main/java/ReverseInteger.java
+++ Java/src/main/java/ReverseInteger.java
@@ -1,15 +1,15 @@
 class ReverseInteger {
-    public static int reverse(int x) {
-        long result = 0;
+  public static int reverse(int x) {
+    long result = 0;
 
-        while (x != 0) {
-            int lastDigit = x % 10;
-            result = (result * 10) + lastDigit;
-            if (result < Integer.MIN_VALUE || result > Integer.MAX_VALUE) {
-                return 0;
-            }
-            x /= 10;
-        }
-        return (int) result;
+    while (x != 0) {
+      int lastDigit = x % 10;
+      result = (result * 10) + lastDigit;
+      if (result < Integer.MIN_VALUE || result > Integer.MAX_VALUE) {
+        return 0;
+      }
+      x /= 10;
     }
+    return (int) result;
+  }
 }
