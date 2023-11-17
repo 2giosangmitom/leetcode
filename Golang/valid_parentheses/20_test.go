adc7716dd14693bd407e37a5109f54dfb480ca5c
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-11-17 18:46:51 +0700

    go: add 20

---
+++ Golang/valid_parentheses/20_test.go
@@ -0,0 +1,28 @@
+package validparentheses
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_20(t *testing.T) {
+	cases := []struct {
+		s    string
+		want bool
+	}{
+		{s: "()", want: true},
+		{s: "()[]{}", want: true},
+		{s: "(]", want: false},
+		{s: "){", want: false},
+    {s: "][]", want: false},
+	}
+
+	for i, tt := range cases {
+		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
+			result := isValid(tt.s)
+			assert.Equal(t, tt.want, result)
+		})
+	}
+}
