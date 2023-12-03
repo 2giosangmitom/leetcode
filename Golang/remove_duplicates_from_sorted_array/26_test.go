dc3646f5b53651878160c31d4b952bcaf4757695
Vo Quang Chien <voquangchien.dev@proton.me>
2023-12-03 07:55:49 +0700

    go: add 26

---
+++ Golang/remove_duplicates_from_sorted_array/26_test.go
@@ -0,0 +1,27 @@
+package removeduplicatesfromsortedarray
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_26(t *testing.T) {
+	cases := []struct {
+		nums    []int
+		want    int
+		wantArr []int
+	}{
+		{nums: []int{1, 1, 2}, want: 2, wantArr: []int{1, 2}},
+		{nums: []int{0, 0, 1, 1, 1, 1, 2, 3, 4}, want: 5, wantArr: []int{0, 1, 2, 3, 4}},
+	}
+
+	for i, tt := range cases {
+		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
+			result := removeDuplicates(tt.nums)
+			assert.Equal(t, tt.want, result)
+			assert.Equal(t, tt.wantArr, tt.nums[:len(tt.wantArr)])
+		})
+	}
+}
