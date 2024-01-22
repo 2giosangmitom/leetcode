8b4e3f61d31625a5a8b451fbae51e80ff90bedb2
Vo Quang Chien <voquangchien.dev@proton.me>
2024-01-22 13:43:41 +0700

    c#: RomanToInteger

---
+++ CSharp/Tests/RomanToInteger.cs
@@ -0,0 +1,25 @@
+using Solutions;
+
+namespace Tests;
+
+public class RomanToIntegerTest
+{
+    [Fact]
+    public void RomanToIntegerTest1()
+    {
+        List<(string roman, int want)> TestCases =
+        [
+            (roman: "III", want: 3),
+            (roman: "LVIII", want: 58),
+            (roman: "MCMXCIV", want: 1994),
+            (roman: "XXIV", want: 24),
+            (roman: "LLVMR", want: -1),
+        ];
+
+        foreach ((string roman, int want) in TestCases)
+        {
+            int result = RomanToInteger.RomanToInt(roman);
+            Assert.Equal(want, result);
+        }
+    }
+}
