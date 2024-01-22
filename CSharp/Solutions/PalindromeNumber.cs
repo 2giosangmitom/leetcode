7b0cf3fd7584d635bc1b1e36da6115339123e8f5
Vo Quang Chien <voquangchien.dev@proton.me>
2024-01-22 13:43:02 +0700

    c#: PalindromeNumber

---
+++ CSharp/Solutions/PalindromeNumber.cs
@@ -0,0 +1,26 @@
+namespace Solutions;
+
+public class PalindromeNumber
+{
+    public static bool IsPalindrome(int x)
+    {
+        if (x < 0)
+        {
+            return false;
+        }
+
+        static int reverse(int n)
+        {
+            int result = 0;
+            while (n != 0)
+            {
+                int lastDigit = n % 10;
+                result = (result * 10) + lastDigit;
+                n /= 10;
+            }
+            return result;
+        }
+
+        return reverse(x) == x;
+    }
+}
---
+++ CSharp/Tests/PalindromeNumber.cs
@@ -0,0 +1,25 @@
+using Solutions;
+
+namespace Tests;
+
+public class PalindromeNumberTest
+{
+    [Fact]
+    public void Test()
+    {
+        List<(int num, bool want)> TestCases =
+        [
+            (num: -10, want: false),
+            (num: 5, want: true),
+            (num: 121, want: true),
+            (num: 321, want: false),
+            (num: 111, want: true)
+        ];
+
+        foreach ((int num, bool want) in TestCases)
+        {
+            bool result = PalindromeNumber.IsPalindrome(num);
+            Assert.Equal(want, result);
+        }
+    }
+}
