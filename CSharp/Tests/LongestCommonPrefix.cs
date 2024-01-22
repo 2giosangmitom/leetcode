3dc51ae99d878ae9bdf31cd7990951725f75591d
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-22 13:42:53 +0700

    c#: LongestCommonPrefix

---
+++ CSharp/Tests/LongestCommonPrefix.cs
@@ -0,0 +1,23 @@
+using Solutions;
+
+namespace Tests;
+
+public class LongestCommonPrefixTest
+{
+    [Fact]
+    public void Test()
+    {
+        List<(string[] strs, string want)> TestCases =
+        [
+            (new string[] { "flower", "flow", "flight" }, "fl"),
+            (new string[] { "dog", "racecar", "car" }, ""),
+            (new string[] { "chi", "chien", "chau" }, "ch")
+        ];
+
+        foreach ((string[] strs, string want) in TestCases)
+        {
+            string result = LongestCommonPrefix.LongestCommonPrefix1(strs);
+            Assert.Equal(want, result);
+        }
+    }
+}
