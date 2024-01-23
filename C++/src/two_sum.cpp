ffb679b496d9d925a2bf3f1f2c6c988ea90c0d81
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-24 05:06:59 +0700

    chore: format code

--- C++/src/two_sum.cpp
+++ C++/src/two_sum.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 class Solution {
- public:
+public:
   vector<int> twoSum(vector<int> &nums, int target) {
     map<int, int> map;
     for (int i = 0; i < nums.size(); i++) {
