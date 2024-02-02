3c4b5358126275b5cd4d9589ba89fede4b8629dc
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-02 16:15:45 +0700

    ts: add 2

---
+++ TypeScript/add_two_numbers/2.test.ts
@@ -0,0 +1,37 @@
+import { assertEquals } from "../deps.ts";
+import { addTwoNumbers, ListNode } from "./2.ts";
+
+interface tt {
+  l1: ListNode;
+  l2: ListNode;
+  want: ListNode;
+}
+
+const cases: tt[] = [
+  {
+    l1: new ListNode(2, new ListNode(4, new ListNode(3))),
+    l2: new ListNode(5, new ListNode(6, new ListNode(4))),
+    want: new ListNode(7, new ListNode(0, new ListNode(8))),
+  },
+  {
+    l1: new ListNode(
+      9,
+      new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))),
+    ),
+    l2: new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))),
+    want: new ListNode(
+      8,
+      new ListNode(
+        9,
+        new ListNode(9, new ListNode(9, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(1)))))),
+      ),
+    ),
+  },
+];
+
+for (const t of cases) {
+  Deno.test(`addTwoNumbers(${JSON.stringify(t.l1)}, ${JSON.stringify(t.l2)}, ${JSON.stringify(t.want)})`, () => {
+    const result = addTwoNumbers(t.l1, t.l2);
+    assertEquals(result, t.want);
+  });
+}
--- TypeScript/two_sum/1.test.ts
+++ TypeScript/two_sum/1.test.ts
@@ -2,26 +2,26 @@ import { assertEquals } from "../deps.ts";
 import { twoSum, twoSum2 } from "./1.ts";
 
 interface tt {
-	nums: number[];
-	target: number;
-	want: number[];
+  nums: number[];
+  target: number;
+  want: number[];
 }
 
 const cases: tt[] = [
-	{ nums: [2, 7, 11, 15], target: 9, want: [0, 1] },
-	{ nums: [3, 2, 4], target: 6, want: [1, 2] },
-	{ nums: [3, 3], target: 6, want: [0, 1] },
-	{ nums: [2, 3, 4, 1, 25, 8], target: 30, want: [-1] },
+  { nums: [2, 7, 11, 15], target: 9, want: [0, 1] },
+  { nums: [3, 2, 4], target: 6, want: [1, 2] },
+  { nums: [3, 3], target: 6, want: [0, 1] },
+  { nums: [2, 3, 4, 1, 25, 8], target: 30, want: [-1] },
 ];
 
 for (const t of cases) {
-	Deno.test(`twoSum(${JSON.stringify(t.nums)}, ${t.target})`, () => {
-		const result = twoSum(t.nums, t.target);
-		assertEquals(result, t.want);
-	});
+  Deno.test(`twoSum(${JSON.stringify(t.nums)}, ${t.target})`, () => {
+    const result = twoSum(t.nums, t.target);
+    assertEquals(result, t.want);
+  });
 
-	Deno.test(`twoSum2(${JSON.stringify(t.nums)}, ${t.target})`, () => {
-		const result = twoSum2(t.nums, t.target);
-		assertEquals(result, t.want);
-	});
+  Deno.test(`twoSum2(${JSON.stringify(t.nums)}, ${t.target})`, () => {
+    const result = twoSum2(t.nums, t.target);
+    assertEquals(result, t.want);
+  });
 }
