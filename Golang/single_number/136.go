21436d25260c3c5448e94b92ff818c7eff6d2d61
Vo Quang Chien <voquangchien.dev@proton.me>
2024-02-03 21:03:09 +0700

    go: add 136 - Single Number

---
+++ Golang/single_number/136.go
@@ -0,0 +1,20 @@
+package singlenumber
+
+func singleNumber(nums []int) int {
+	hash_map := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		if value, ok := hash_map[nums[i]]; ok {
+			hash_map[nums[i]] = value + 1
+		} else {
+			hash_map[nums[i]] = 1
+		}
+	}
+
+	for i := 0; i < len(nums); i++ {
+		if hash_map[nums[i]] == 1 {
+			return nums[i]
+		}
+	}
+
+	return -1
+}
