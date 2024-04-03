3c03190104fb58c9991afb2ab33b1098d429f52a
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-04-03 21:24:10 +0700

    java: add more test case for binary search

--- Java/lib/src/test/java/org/leetcode/BinarySearchTest.java
+++ Java/lib/src/test/java/org/leetcode/BinarySearchTest.java
@@ -6,8 +6,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BinarySearchTest {
     @Test
-    void Test() {
+    void Test1() {
         Solution s = new Solution();
         assertEquals(4, s.search(new int[] { -1, 0, 3, 5, 9, 12 }, 9));
+    }
+
+    @Test
+    void Test2() {
+        Solution s = new Solution();
+        assertEquals(-1, s.search(new int[] { -1, 0, 3, 5, 9, 12 }, 2));
     }
 }
