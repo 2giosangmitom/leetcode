92c9e77eedbad0032b4017222699524965c24a49
Vo Quang Chien <voquangchien.dev@proton.me>
2024-04-07 16:43:33 +0700

    chore(java): format code with `google-java-format`

--- Java/lib/src/main/java/org/leetcode/TwoSum.java
+++ Java/lib/src/main/java/org/leetcode/TwoSum.java
@@ -3,28 +3,28 @@ package org.leetcode;
 import java.util.HashMap;
 
 public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < nums.length; i++) {
-            int neededNumber = target - nums[i];
-            if (map.containsKey(neededNumber)) {
-                return new int[] { map.get(neededNumber), i };
-            } else {
-                map.put(nums[i], i);
-            }
-        }
-        return new int[] { -1 };
+  public int[] twoSum(int[] nums, int target) {
+    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for (int i = 0; i < nums.length; i++) {
+      int neededNumber = target - nums[i];
+      if (map.containsKey(neededNumber)) {
+        return new int[] {map.get(neededNumber), i};
+      } else {
+        map.put(nums[i], i);
+      }
     }
+    return new int[] {-1};
+  }
 
-    public int[] twoSum2(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            int neededNumber = target - nums[i];
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] == neededNumber) {
-                    return new int[] { i, j };
-                }
-            }
+  public int[] twoSum2(int[] nums, int target) {
+    for (int i = 0; i < nums.length; i++) {
+      int neededNumber = target - nums[i];
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[j] == neededNumber) {
+          return new int[] {i, j};
         }
-        return new int[] { -1 };
+      }
     }
+    return new int[] {-1};
+  }
 }
--- Java/lib/src/test/java/org/leetcode/BinarySearchTest.java
+++ Java/lib/src/test/java/org/leetcode/BinarySearchTest.java
@@ -1,42 +1,38 @@
 package org.leetcode;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.Arrays;
-
 class BinarySearchTest {
-    private BinarySearch binarySearch;
+  private BinarySearch binarySearch;
 
-    @BeforeEach
-    void setUp() {
-        binarySearch = new BinarySearch();
-    }
+  @BeforeEach
+  void setUp() {
+    binarySearch = new BinarySearch();
+  }
 
-    @Test
-    @DisplayName("Search for existing element")
-    void testExistingElement() {
-        assertEquals(4, binarySearch.search(new int[] { -1, 0, 3, 5, 9, 12 }, 9));
-    }
+  @Test
+  @DisplayName("Search for existing element")
+  void testExistingElement() {
+    assertEquals(4, binarySearch.search(new int[] {-1, 0, 3, 5, 9, 12}, 9));
+  }
 
-    @Test
-    @DisplayName("Search for non-existing element")
-    void testNonExistingElement() {
-        assertEquals(-1, binarySearch.search(new int[] { -1, 0, 3, 5, 9, 12 }, 2));
-    }
+  @Test
+  @DisplayName("Search for non-existing element")
+  void testNonExistingElement() {
+    assertEquals(-1, binarySearch.search(new int[] {-1, 0, 3, 5, 9, 12}, 2));
+  }
 
-    @ParameterizedTest(name = "Search for {1} in array [{2}]")
-    @CsvSource({
-            "-1, -1, '0, 3, 5, 9, 12'",
-            "3, 9, '0, 3, 5, 9, 12'"
-    })
-    void testSearchVariants(int expectedIndex, int target, String array) {
-        int[] arr = Arrays.stream(array.split(", ")).mapToInt(Integer::parseInt).toArray();
-        assertEquals(expectedIndex, binarySearch.search(arr, target));
-    }
+  @ParameterizedTest(name = "Search for {1} in array [{2}]")
+  @CsvSource({"-1, -1, '0, 3, 5, 9, 12'", "3, 9, '0, 3, 5, 9, 12'"})
+  void testSearchVariants(int expectedIndex, int target, String array) {
+    int[] arr = Arrays.stream(array.split(", ")).mapToInt(Integer::parseInt).toArray();
+    assertEquals(expectedIndex, binarySearch.search(arr, target));
+  }
 }
