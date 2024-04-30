dd73d9ba74fd084d10cc6623fdeeb27ecb9068e7
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-04-30 23:05:23 +0700

    update

--- Rust/tests/reverse_integer.rs
+++ Rust/tests/reverse_integer.rs
@@ -3,7 +3,7 @@ mod tests {
     use leetcode::reverse_integer::*;
 
     #[test]
-    fn test_reverse_integer_case1() {
+    fn test_reverse_positive_number() {
         let x = 123;
         let want = 321;
 
@@ -13,7 +13,7 @@ mod tests {
     }
 
     #[test]
-    fn test_reverse_integer_case2() {
+    fn test_reverse_negative_number() {
         let x = -123;
         let want = -321;
 
@@ -23,7 +23,7 @@ mod tests {
     }
 
     #[test]
-    fn test_reverse_integer_case3() {
+    fn test_reverse_zero_ending_number() {
         let x = 120;
         let want = 21;
 
@@ -33,7 +33,7 @@ mod tests {
     }
 
     #[test]
-    fn test_reverse_integer_case4() {
+    fn test_reverse_large_number() {
         let x = 1534236469;
         let want = 0;
 
@@ -43,7 +43,7 @@ mod tests {
     }
 
     #[test]
-    fn test_reverse_integer_case5() {
+    fn test_reverse_negative_overflow() {
         let x = -2147483648;
         let want = 0;
 
@@ -53,7 +53,7 @@ mod tests {
     }
 
     #[test]
-    fn test_reverse_integer_case6() {
+    fn test_reverse_positive_overflow() {
         let x = 2147412349;
         let want = 0;
 
@@ -63,7 +63,7 @@ mod tests {
     }
 
     #[test]
-    fn test_reverse_integer_case7() {
+    fn test_reverse_trailing_zeros() {
         let x = 900000;
         let want = 9;
 
