02557df8ada14201479c2dbc1cf7268926fb96ac
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-05-19 16:23:02 +0700

    update structure

--- TypeScript/benches/two_sum.bench.ts
+++ TypeScript/benches/two_sum.bench.ts
@@ -1,17 +1,5 @@
 import { twoSum, twoSum2 } from "../src/two_sum.ts";
-
-interface TestCase {
-  nums: number[];
-  target: number;
-  want: number[];
-}
-
-const testCases: TestCase[] = [
-  { nums: [2, 7, 11, 15], target: 9, want: [0, 1] },
-  { nums: [3, 2, 4], target: 6, want: [1, 2] },
-  { nums: [3, 3], target: 6, want: [0, 1] },
-  { nums: [2, 3, 4, 1, 25, 8], target: 30, want: [-1] },
-];
+import { testCases } from "../tests/two_sum.test.ts";
 
 Deno.bench("Benchmark twoSum", { group: "two_sum" }, () => {
   for (const testCase of testCases) {
