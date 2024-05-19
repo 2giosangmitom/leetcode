0a412e1470412f92249bbd6098b2e804d4364ef0
Vo Quang Chien <voquangchien.dev@proton.me>
2024-05-19 16:23:02 +0700

    update structure

--- TypeScript/tests/two_sum.test.ts
+++ TypeScript/tests/two_sum.test.ts
@@ -7,7 +7,7 @@ interface TestCase {
   want: number[];
 }
 
-const testCases: TestCase[] = [
+export const testCases: TestCase[] = [
   { nums: [2, 7, 11, 15], target: 9, want: [0, 1] },
   { nums: [3, 2, 4], target: 6, want: [1, 2] },
   { nums: [3, 3], target: 6, want: [0, 1] },
