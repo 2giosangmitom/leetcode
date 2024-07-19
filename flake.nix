921d8498c01438db031213da419904299bdc4f53
Vo Quang Chien <voquangchien.dev@proton.me>
2024-07-19 21:42:17 +0700

    update nix flakes

--- flake.nix
+++ flake.nix
@@ -4,30 +4,20 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {inherit system;};
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             go
             deno
-            nil
-            alejandra
             cargo
             rustc
             rustfmt
             rust-analyzer
             clippy
-            nodejs_20
           ];
         };
-        formatter = pkgs.alejandra;
-      }
-    );
+      });
 }
