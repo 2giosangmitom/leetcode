7dfd92af0732c231bb0552096f10d785a3f97bbc
Vo Quang Chien <voquangchien.dev@proton.me>
2024-10-08 10:48:48 +0700

    js: add 49

---
+++ JavaScript/tests/49.test.js
@@ -0,0 +1,38 @@
+import { describe, test } from "node:test";
+import { groupAnagrams } from "../src/49.js";
+
+/**
+ * @param {string[][]} result
+ * @param {string[][]} expected
+ */
+function assertEqualAnagrams(result, expected) {
+	if (result.length !== expected.length) {
+		throw new Error(
+			`Assertion failed!\n Expect: ${JSON.stringify(expected)}\n Actual: ${JSON.stringify(result)}`,
+		);
+	}
+}
+
+const cases = [
+	{
+		strs: ["eat", "tea", "tan", "ate", "nat", "bat"],
+		want: [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]],
+	},
+	{
+		strs: ["a"],
+		want: [["a"]],
+	},
+	{
+		strs: [""],
+		want: [[""]],
+	},
+];
+
+describe("49. Group Anagrams", () => {
+	for (let i = 0; i < cases.length; i++) {
+		test(`case ${i + 1}`, () => {
+			const result = groupAnagrams(cases[i].strs);
+			assertEqualAnagrams(result, cases[i].want);
+		});
+	}
+});
