423d6b59f8152a667bc9e8be01aed4008c77085f
Vo Quang chien <voquangchien.dev@proton.me>
2024-11-18 10:25:09 +0700

    test(java): strictly check nums array in remove duplicates 2

--- Java/lib/src/test/java/leetcode/RemoveDuplicates2/RemoveDuplicates2Test.java
+++ Java/lib/src/test/java/leetcode/RemoveDuplicates2/RemoveDuplicates2Test.java
@@ -1,7 +1,9 @@
 package leetcode.RemoveDuplicates2;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,27 +13,33 @@ class RemoveDuplicates2Test {
   @Test
   @DisplayName("positive numbers only")
   void test1() {
-    int[] nums = { 1, 1, 1, 2, 2, 3 };
+    int[] nums = {1, 1, 1, 2, 2, 3};
     int expected = 5;
+    int[] expectedNums = {1, 1, 2, 2, 3};
     int actual = solution.removeDuplicates(nums);
     assertEquals(expected, actual);
+    assertArrayEquals(expectedNums, Arrays.copyOfRange(nums, 0, expected));
   }
 
   @Test
   @DisplayName("apear randomly")
   void test2() {
-    int[] nums = { 0, 0, 1, 1, 1, 1, 2, 3, 3 };
+    int[] nums = {0, 0, 1, 1, 1, 1, 2, 3, 3};
     int expected = 7;
+    int[] expectedNums = {0, 0, 1, 1, 2, 3, 3};
     int actual = solution.removeDuplicates(nums);
     assertEquals(expected, actual);
+    assertArrayEquals(expectedNums, Arrays.copyOfRange(nums, 0, expected));
   }
 
   @Test
   @DisplayName("has negative numbers")
   void test3() {
-    int[] nums = { -1, -1, 0, 0, 1, 1, 1, 2, 3, 3 };
+    int[] nums = {-1, -1, 0, 0, 1, 1, 1, 2, 3, 3};
     int expected = 9;
+    int[] expectedNums = {-1, -1, 0, 0, 1, 1, 2, 3, 3};
     int actual = solution.removeDuplicates(nums);
     assertEquals(expected, actual);
+    assertArrayEquals(expectedNums, Arrays.copyOfRange(nums, 0, expected));
   }
 }
