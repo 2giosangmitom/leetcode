a774e405ebab54b118bac3f0a00698fd17bc7b5f
Vo Quang Chien <voquangchien.dev@proton.me>
2024-12-19 21:04:22 +0700

    simplify code

--- C++/src/add_binary.hpp
+++ C++/src/add_binary.hpp
@@ -17,14 +17,13 @@ public:
     int carry = 0;
 
     string result;
-    result.reserve(max(len_a, len_b) + 1);
 
     while (i >= 0 || j >= 0 || carry > 0) {
       int bit1 = (i >= 0) ? a[i--] - '0' : 0;
       int bit2 = (j >= 0) ? b[j--] - '0' : 0;
 
       int sum = bit1 + bit2 + carry;
-      result.push_back((sum % 2) + '0');
+      result += (sum % 2) + '0';
       carry = sum / 2;
     }
 
